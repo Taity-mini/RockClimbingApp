a707dda80749f08ae6f898803ea15c10f9928b8c
Taity-mini <1504693@rgu.ac.uk>
2017-04-12 00:25:06 +0100

    Navigation Fragments  *Added main page fragments for app  *Started home screen design

---
+++ app/src/main/java/andrewtait1504693/rockclimbingapp/Locations.java
@@ -0,0 +1,29 @@
+package andrewtait1504693.rockclimbingapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Locations extends Fragment {
+
+
+    public Locations() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_locations, container, false);
+    }
+
+}
--- app/src/main/java/andrewtait1504693/rockclimbingapp/MainActivity.java
+++ app/src/main/java/andrewtait1504693/rockclimbingapp/MainActivity.java
@@ -1,15 +1,20 @@
 package andrewtait1504693.rockclimbingapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
-
+    FragmentTransaction fragmentTransaction;
+    NavigationView navigationView;
     Toolbar toolbar;
 
     @Override
@@ -27,10 +32,63 @@ public class MainActivity extends AppCompatActivity {
                 R.string.drawer_open,
                 R.string.drawer_close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.main_container, new Home());
+        fragmentTransaction.commit();
+        getSupportActionBar().setTitle("Home");
+
+        //Fragment navigation  switch from drawer menu
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.home_id:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new Home());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Home");
+                        item.setCheckable(true);
+                        drawerLayout.closeDrawers();
+                        break;
+
+
+                    case R.id.routes_id:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new Routes());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Routes");
+                        item.setCheckable(true);
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.locations_id:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new Locations());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Locations");
+                        item.setCheckable(true);
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.preferences_id:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new Preferences());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Preferences");
+                        item.setCheckable(true);
+                        drawerLayout.closeDrawers();
+                        break;
+                }
+                return false;
+            }
+        });
+
 
     }
 
-    protected void onPostCreate(Bundle savedInstanceState){
+    protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         actionBarDrawerToggle.syncState();
     }
