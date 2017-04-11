059027b33047a73ef5a6b63857a7d68353dfeb14
Taity-mini <1504693@rgu.ac.uk>
2017-04-11 19:25:57 +0100

    Hamburger Menu Working *Added items to string values

--- app/src/main/java/andrewtait1504693/rockclimbingapp/MainActivity.java
+++ app/src/main/java/andrewtait1504693/rockclimbingapp/MainActivity.java
@@ -1,17 +1,37 @@
 package andrewtait1504693.rockclimbingapp;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+    }
+
+    protected void onPostCreate(Bundle savedInstanceState){
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
     }
 }
